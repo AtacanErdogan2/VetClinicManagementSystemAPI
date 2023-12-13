3339ab7598132d0291acc922be8a5620a9a981f0
AtacanErdogan <atacanerdogan2@gmail.com>
2023-12-13 18:41:42 +0300

    .sql added

--- src/main/java/dev/patika/dal/IAvailableDateRepo.java
+++ src/main/java/dev/patika/dal/IAvailableDateRepo.java
@@ -3,6 +3,8 @@ package dev.patika.dal;
 import dev.patika.entity.AvailableDate;
 import dev.patika.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,12 +12,19 @@ import java.util.Optional;
 
 @Repository
 public interface IAvailableDateRepo extends JpaRepository<AvailableDate, Long> {
-   Optional<AvailableDate> findByDate(LocalDate date);
+    Optional<AvailableDate> findByDate(LocalDate date);
 
 //   @Query("SELECT availableDate FROM AvailableDate JOIN Doctor on AvailableDate. ")
-   // SELECT available_date FROM available_date WHERE doctor_id = ?
+    // SELECT available_date FROM available_date WHERE doctor_id = ?
 //   @Query("SELECT available_date FROM available_date WHERE doctor_id = ?1")
 //   List<AvailableDateResponse> findByDoctorId(Long id, org.springframework.data.domain.Pageable pageable);
 
-   Optional<AvailableDate> findByDoctorAndDate(Doctor doctor, LocalDate date);
+    Optional<AvailableDate> findByDoctorAndDate(Doctor doctor, LocalDate date);
+
+//    @Query("SELECT ad FROM AvailableDate ad WHERE ad.doctor.id = :id " +
+//            "AND ad.date = :date")
+//    AvailableDate findByDoctorIdAndDate(
+//            @Param("id") Long id,
+//            @Param("date") LocalDate date
+//    );
 }
